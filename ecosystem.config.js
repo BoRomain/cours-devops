e54a12cec1d2607056ef052abb7d4b96349148c7
BoRomain <romain.bouffandeau@gmail.com>
2026-04-21 14:16:38 +0200

    feat pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [
+    {
+      name: "next-app",
+      script: "npx",
+      args: "next dev -H 0.0.0.0",
+      cwd: "/home/azureuser/cours-devops",
+    },
+  ],
+};
